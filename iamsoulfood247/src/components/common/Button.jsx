d77b40f55bd285692511a10fbf8f1235d280407a
Jon Narine <jon.wolf.narine@gmail.com>
2025-09-11 16:28:28 -0400

    hk: removes boilerplate code in App.jsx, add: theme.js, feat: Button.jsx added to common comps

--- iamsoulfood247/src/components/common/Button.jsx
+++ iamsoulfood247/src/components/common/Button.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import styled from "styled-components";
 
-const Button = () => {
+const Button = ({
+  name,
+}) => {
   return (
-    <div>Button</div>
-  )
-}
+    <ButtonStyled>{name}</ButtonStyled>
+  );
+};
 
-export default Button
+const ButtonStyled = styled.button`
+  border: 1px solid #000;
+  border-radius: 1.5rem;
+  background: #222;
+  color: #fff;
+  cursor: pointer;
+  padding: ${({ theme }) => `${theme.spacing.sm} ${theme.spacing.lg}`};
+  font-size: ${({ theme }) => theme.fontSize.md};
+  transition: padding 0.3s ease;
+
+  &:hover {
+    padding: ${({ theme }) => `${theme.spacing.sm} ${theme.spacing.xl}`};
+    letter-spacing: 0.15rem;
+  };
+`;
+
+export default Button;
---
+++ iamsoulfood247/src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  spacing: {
+    sm: '0.5rem',
+    md: '1rem',
+    lg: '2rem',
+    xl: '4rem',
+  },
+  fontSize: {
+    sm: '0.875rem',
+    md: '1rem',
+    lg: '1.25rem',
+  },
+  colors: {
+    primary: '#0070f3',
+    secondary: '#1c1c1e',
+    background: '#f5f5f5',
+    text: '#333',
+  },
+};
