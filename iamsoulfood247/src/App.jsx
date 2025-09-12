1a55d2caf2a685f3093367b258fc1ace2ac9607a
Jon Narine <jon.wolf.narine@gmail.com>
2025-09-11 20:23:20 -0400

    add: dynamic toggle between base button and nav button

--- iamsoulfood247/src/App.jsx
+++ iamsoulfood247/src/App.jsx
@@ -2,7 +2,8 @@ import Button from "./components/common/Button";
 function App() {
   return (
     <>
-      <Button name='BUTTON' border={false}/>
+      <Button name='BUTTON' border={false} isNavButton={false}/>
+      <Button name='BUTTON' border={false} isNavButton />
     </>
   );
 };
--- iamsoulfood247/src/components/common/Button.jsx
+++ iamsoulfood247/src/components/common/Button.jsx
@@ -2,26 +2,31 @@ import styled from "styled-components";
 
 const Button = ({
   name,
-  border = true,
+  border = false,
+  isNavButton = false,
 }) => {
   return (
-    <ButtonStyled border={border}>{name}</ButtonStyled>
+    <ButtonStyled border={border} isNavButton={isNavButton}>{name}</ButtonStyled>
   );
 };
 
 const ButtonStyled = styled.button`
   border: ${({ border }) => border ? '1px solid #000' : 'none'};
   border-radius: 1.5rem;
-  background: #222;
-  color: #fff;
+  background:${({ isNavButton }) => isNavButton ? 'none' : '#222'}; ;
+  color: ${({ isNavButton }) => isNavButton ? '#222' : '#fff'};
   cursor: pointer;
-  padding: ${({ theme }) => `${theme.spacing.sm} ${theme.spacing.lg}`};
+  font-weight: ${({ isNavButton }) => isNavButton ? '600' : '500'};
   font-size: ${({ theme }) => theme.fontSize.md};
+  padding: ${({ theme }) => `${theme.spacing.sm} ${theme.spacing.lg}`};
   transition: padding 0.3s ease;
 
   &:hover {
-    padding: ${({ theme }) => `${theme.spacing.sm} ${theme.spacing.xl}`};
-    letter-spacing: 0.15rem;
+    color: ${({ isNavButton }) => isNavButton ? 'grey' : '#fff'};
+    letter-spacing: ${({ isNavButton }) => isNavButton ? '0' : '0.15rem'};
+    padding: ${({ theme, isNavButton }) => isNavButton
+      ? `${theme.spacing.sm} ${theme.spacing.lg}`
+      : `${theme.spacing.sm} ${theme.spacing.xl}`};
   };
 `;
 
