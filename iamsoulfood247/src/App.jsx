176a0e0e1b02076b321b84ccb7ace01a8b3c76b5
Jon Narine <jon.wolf.narine@gmail.com>
2025-09-19 02:52:54 -0400

    add: Text.jsx in common for all paragraph text

--- iamsoulfood247/src/App.jsx
+++ iamsoulfood247/src/App.jsx
@@ -1,5 +1,6 @@
 import Button from "./components/common/Button";
 import Input from "./components/common/Input";
+import Text from "./components/common/Text";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Button name='BUTTON' border={false} isnavbutton={false} />
       <Button name='BUTTON' border={false} isnavbutton />
       <Input />
+      <Text content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." />
     </>
   );
 };
