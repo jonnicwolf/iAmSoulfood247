d99daf79fad40cac72a37405471fa7fa1eab33e3
Jon Narine <jon.wolf.narine@gmail.com>
2025-10-15 04:29:01 -0400

    add: Home page responsiveness

--- iamsoulfood247/src/pages/Home.jsx
+++ iamsoulfood247/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             src="https://dl.dropboxusercontent.com/scl/fi/5761osx5w55gqeakjwjlw/iAmSoulfood247_logo.png?rlkey=hw7vcgy24mfj9712wdgu16oqf&st=qxjum5u0"
             alt="iAmSoulfood247 Logo"
             style={{ width: "400px", marginBottom: "20px" }}
-          />
+            />
           <Text
             content={"IAMSOULFOOD247.COM"}
             textSize="xxl"
@@ -23,13 +23,7 @@ const Home = () => {
 
       <ContentCard>
         <ConceptContent>
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            gap: '5rem',
-            maxWidth: "40%" }}>
+          <TextWrapper>
             <Text content={"OUR CONCEPT"} textSize="xxl" color="surface" bold />
             <Text
               content={"iAmSoulfood247 2 Go aims to revolutionize the meal prep industry by offering delicious, healthy soul food options that cater to busy individuals seeking nutritious meals without sacrificing flavor. Our mission is to make soul food accessible, affordable, and health-conscious, promoting a balanced lifestyle while celebrating the rich culinary heritage of soul food."}
@@ -42,27 +36,17 @@ const Home = () => {
               isnavbutton
               color="textLight"
               backgroundColor="surface" />
-          </div>
+          </TextWrapper>
           <Img
             src="https://dl.dropboxusercontent.com/scl/fi/jt7zghw8189lio026no70/soulfood.jpg?rlkey=ww72w9lurnz692r53volvt77p&st=0msnug71"
             alt=""
             />
         </ConceptContent>
       </ContentCard>
-
+ 
       <ContentCard>
-        <ConceptContent>
-          <Img
-            src="https://dl.dropboxusercontent.com/scl/fi/jevkllqaur7xrgkizv4ij/catering.jpg?rlkey=ss7svgetibrf8lrwi514e7zsp&st=chhe2cpw"
-            alt=""
-             />
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            gap: '5rem',
-            maxWidth: "40%" }}>
+        <ConceptContent flip>
+          <TextWrapper>
             <Text content={"LET US CATER"} textSize="xxl" color="surface" bold/>
             <Text
               content={"iAmSoulfood247 Catering brings the heart of soulful cooking to your gatherings, combining rich flavors with health-conscious preparation. Whether it’s a corporate lunch, family celebration, or large-scale event, we deliver meals that are both comforting and nourishing. Our goal is to make authentic soul food accessible and memorable for every occasion—celebrating tradition while supporting modern lifestyles."}
@@ -76,19 +60,17 @@ const Home = () => {
               isnavbutton
               color="textLight"
               backgroundColor="surface" />
-          </div>
+          </TextWrapper>
+          <Img
+            src="https://dl.dropboxusercontent.com/scl/fi/jevkllqaur7xrgkizv4ij/catering.jpg?rlkey=ss7svgetibrf8lrwi514e7zsp&st=chhe2cpw"
+            alt=""
+            />
         </ConceptContent>
       </ContentCard>
 
       <ContentCard>
         <ConceptContent>
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            gap: '5rem',
-            maxWidth: "40%" }}>
+          <TextWrapper>
             <Text content={"MEAL PLANS"} textSize="xxl" color="surface" bold/>
             <Text
               content={"iAmSoulfood247 Meal Plans are designed for busy individuals who want soulful flavor without compromising health or convenience. Our rotating menus balance tradition with nutrition, offering portioned, ready-to-enjoy meals that fit seamlessly into your lifestyle. With flexible options to match your goals, we make eating well simple, affordable, and full of soul."}
@@ -102,7 +84,7 @@ const Home = () => {
               isnavbutton
               color="textLight"
               backgroundColor="surface" />
-          </div>
+          </TextWrapper>
           <Img
             src="https://dl.dropboxusercontent.com/scl/fi/ekl4xx8i8pq7hdtn5zpwi/mealPlan.jpg?rlkey=e93b1s5ysl1t3wabkfcvii1ps&st=qto52op8"
             alt=""
@@ -120,6 +102,10 @@ const PageWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 2rem;
+
+  @media (max-width: 768px) {
+    gap: 0;
+  }
 `;
 
 const Content = styled.div`
@@ -128,18 +114,51 @@ const Content = styled.div`
   align-items: center;
   padding: 20px;
 `;
+
 const TitleContent = styled(Content)``;
 const ContentCard = styled(Card)`
   box-shadow: none;
 `;
+
 const ConceptContent = styled(Content)`
   background: ${({ theme }) => theme.colors.textLight};
-  flex-direction: row;
+  display: flex;
+  flex-direction: ${({ flip }) => (flip ? 'row-reverse' : 'row')};
   justify-content: space-around;
+  align-items: center;
   padding: 55px;
+
+  @media (max-width: 768px) {
+    flex-direction: column-reverse !important;
+    padding: 20px;
+  }
 `;
 
 const Img = styled.img`
-  width: 800px;
-  height: 800px;
-`
+  height: 40vh;
+  width: 40vw;
+  height: auto;
+  border-radius: 1rem;
+  object-fit: cover;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    height: auto;
+    margin-bottom: 2rem;
+  }
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5rem;
+  max-width: 40%;
+
+  @media (max-width: 768px) {
+    text-align: center;
+    max-width: 100%;
+    gap: 2rem;
+  }
+`;
