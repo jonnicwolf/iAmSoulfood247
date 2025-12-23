6f497d6ba251d53774a077dbbfad27655b41f7c4
Jon Narine <jon.wolf.narine@gmail.com>
2025-12-23 10:16:50 -0500

    fix: realign title for each form

--- iamsoulfood247/src/pages/Catering.jsx
+++ iamsoulfood247/src/pages/Catering.jsx
@@ -6,24 +6,31 @@ import Text from '../components/common/Text';
 
 import EventType from "../components/molecules/EventType";
 import GuestCount from '../components/molecules/GuestCount';
+import Theme from '../components/molecules/Theme';
 
 const Catering = () => {
   const [step, setStep] = useState(0);
 
   const forms = [
-    {title: <Div2>
+    {title: <Title>
               <svg
               xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke='#f97316' strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-sparkles-icon lucide-sparkles"><path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z"/><path d="M20 2v4"/><path d="M22 4h-4"/><circle cx="4" cy="20" r="2"/></svg>
               <h4>Event Type</h4>
-            </Div2>,
+            </Title>,
       form: EventType
     },
-    {title: <Div2>
+    {title: <Title>
               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#f97316" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-users-icon lucide-users"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><path d="M16 3.128a4 4 0 0 1 0 7.744"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/><circle cx="9" cy="7" r="4"/></svg>
               <h4>Guest Count</h4>
-            </Div2>,
+            </Title>,
       form: GuestCount,
-    }
+    },
+    {title: <Title>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#f97316" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles-icon lucide-sparkles"><path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z"/><path d="M20 2v4"/><path d="M22 4h-4"/><circle cx="4" cy="20" r="2"/></svg>
+              <h4>Theme</h4>
+            </Title>,
+      form: Theme,
+    },
   ];
 
   const CurrentForm = forms[step].form;
@@ -70,6 +77,14 @@ const Div2 = styled.div`
   justify-content: center;
   align-items: center;
   gap: ${({ theme }) => theme.spacing.sm};
+`;
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  padding: ${({ theme }) => theme.spacing.md};
+  padding-left: 0;
+  gap: ${({ theme }) => theme.spacing.sm};
+  justify-content: flex-start;
 `;
 const FormCard = styled(Card)`
   border: 1px solid gray;
